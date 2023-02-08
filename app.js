b187c1df93e67950f246d106ad5d89a9776fb292
Cate <anncatherinedeguia@gmail.com>
2023-02-08 08:32:29 +0800

    Add clear function

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
+let displayScreen = document.querySelector('#display-screen')
+let currentDisplay = document.querySelector('.current-display')
+let resultDisplay = document.querySelector('.result-display')
+
 function add(num1, num2) {
   let totalSum = Number(num1) + Number(num2);
-    console.log(totalSum);
+  resultDisplay.innerHTML = totalSum
 }
 
 function subtract(minuend, subtrahend) {
   let difference = minuend - subtrahend;
-   console.log(difference);
+  resultDisplay.innerHTML = difference
 }
 
 function multiply(num1, num2) {
-  let product = num1 * num2
-  console.log(product)
+  let product = num1 * num2;
+  resultDisplay.innerHTML = product
 }
 
 function divide(divisor, dividend) {
   let quotient = divisor / dividend;
- console.log(quotient);
+  resultDisplay.innerHTML = quotient
 }
 
 function operate() {
@@ -23,10 +27,9 @@ function operate() {
 
   let operation = prompt("enter operation: ").toLowerCase();
   let num2 = Number(prompt("enter second num: "));
-  
-  if (operation === "+") {
-   add(num1, num2)
 
+  if (operation === "+") {
+    add(num1, num2);
   } else if (operation === "-") {
     subtract(num1, num2);
   } else if (operation === "*") {
@@ -38,4 +41,16 @@ function operate() {
   }
 }
 
-// operate();
+  let clearScreen = document.querySelector('#clear')
+  clearScreen.addEventListener('click', () => {
+  currentDisplay.innerHTML = '0'
+  resultDisplay.innerHTML = ''
+  })
+  
+  let btns = document.querySelectorAll('btn');
+
+  for (i of btns) {
+    i.addEventListener('click', function() {
+      console.log(this);
+    });
+  }

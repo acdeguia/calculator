809e1f3e1036b5b6856f18cc2b3d043e63be4f91
Cate <anncatherinedeguia@gmail.com>
2023-02-08 15:12:06 +0800

    Use push()

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ let resultDisplay = document.querySelector('.result-display')
 
 
   let clearScreen = document.querySelector('#clear')
-  clearScreen.addEventListener('click', () => {
-  currentDisplay.innerHTML = '0'
-  resultDisplay.innerHTML = ''
-  })
+
+  function clear(){
+    clearScreen.addEventListener('click', () => {
+      currentDisplay.innerHTML = '0'
+      resultDisplay.innerHTML = ''
+      })
+  }
+  
   
   let btns = document.querySelectorAll('btn');
 
@@ -20,15 +24,20 @@ let resultDisplay = document.querySelector('.result-display')
 
   const buttons = document.getElementById('buttons');
 
+  let array = []
   buttons.addEventListener('click', (event) => {
     const isButton = event.target.className === 'btn';
     if (!isButton) {
       return;
     }
     currentDisplay.innerHTML = event.target.textContent;
+    array.push(event.target.textContent);
+    console.log(array)
+    displayScreen.innerHTML = array
   })
 
   const buttonsOperator = document.getElementById('buttons');
+
 
   buttonsOperator.addEventListener('click', (event) => {
     const isButton = event.target.className === 'btn-operator';
@@ -36,13 +45,14 @@ let resultDisplay = document.querySelector('.result-display')
       return;
     }
     currentDisplay.innerHTML = event.target.textContent;
+    operator = event.target.textContent
   })
 
 let equal = document.getElementById('equal');
 
 
 equal.addEventListener('click', () => {
-  resultDisplay.innerHTML = result
+  operate()
 })
 
 function add(num1, num2) {
@@ -50,25 +60,27 @@ function add(num1, num2) {
 }
 
 function substract(num1, num2) {
-  return num1 - num2
+  resultDisplay.innerHTML = num1 - num2
 }
 
 function multiply(num1, num2) {
-  return num1 * num2
+  resultDisplay.innerHTML = num1 * num2
 }
 
 function divide(num1, num2) {
-  return num1 / num2
+  resultDisplay.innerHTML = num1 / num2
 }
 
 
 function operate(operator, num1, num2) {
   num1 = Number(num1)
-  num2 = Numebr(num2)
+
+  num2 = Number(num2)
+
 
   switch (operator) {
     case '+':
-      return  add(num1, num2)
+      resultDisplay.innerHTML =  add(num1, num2)
     case '-':
       return  substract(num1, num2)
     case 'x':
@@ -79,31 +91,13 @@ function operate(operator, num1, num2) {
     default:
       return null
   }
+ 
 }
-
-// function add(num1, num2) {
-//   let totalSum = Number(num1) + Number(num2);
-//   result = totalSum
-// }
-
-// function subtract(minuend, subtrahend) {
-//   let difference = minuend - subtrahend;
-//   result = difference
-// }
-
-// function multiply(num1, num2) {
-//   let product = num1 * num2;
-//   result = product
-// }
-
-// function divide(divisor, dividend) {
-//   let quotient = divisor / dividend;
-//   result = quotient
-// }
-
 
 function appendNumber(num) {
   if (currentDisplay.textContent === '0')
-    resetScreen()
-  currentOperationScreen.textContent += num
+    clear()
+  currentDisplay.textContent += num
 }
+
+clear()
